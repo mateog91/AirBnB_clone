32711614d90e95582645c3a9db921f113c88e137
SandraCalero <3358@holbertonschool.com>
2021-11-03 15:45:32 -0500

    add file_storage class

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 from uuid import uuid4
 from datetime import datetime
+from models.engine.file_storage import FileStorage
 
 
 class BaseModel:
@@ -16,6 +17,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            FileStorage.storage.save()
         else:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -36,6 +38,7 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
+        FileStorage.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Serializes instances to a JSON file and deserializes JSON file to instances
 """
-
+import json
+from models.base_model import BaseModel
+from datetime import datetime
 
 class FileStorage:
     """Serializes instances to a JSON file and
@@ -19,15 +21,31 @@ class FileStorage:
         """Sets in __objects the obj with key <obj class name>.id
 
         Args:
-            obj ([type]): [description]
+            obj: object to add as value
         """
+        key = "{}.{}".format(self.__class__.__name__, self.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)
         """
+        object_dic = {key: value.to_dict for key, value in FileStorage.__objects}
+        with open(FileStorage.__file_path, mode="w") as file:
+            json.dump(object_dic, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects (only if the JSON file
         (__file_path) exists ; otherwise, do nothing.
         If the file doesn’t exist, no exception should be raised)
         """
+        try:
+            with open(FileStorage.__file_path, mode="r") as file:
+                dict = json.loads(file.read())
+            for key, value in dict.items():
+                if key in ['created_at', 'updated_at']:
+                        setattr(dict, key, datetime.fromisoformat(value))
+                elif key != '__class__':
+                    setattr(dict, key, value)
+                
+        except FileNotFoundError:
+            pass
