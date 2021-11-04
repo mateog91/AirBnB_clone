1a8c8236c63747c9831c29cd1758cc317712eb55
Mateo <mateog91@gmail.com>
2021-11-03 21:26:19 -0500

    ADD: directory test engine and test file

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Test Module for File Storage
+"""
+import unittest
+from models.engine import file_storage
+
+
+class TestBaseModel(unittest.TestCase):
+    """Class to test Base Model
+    """
