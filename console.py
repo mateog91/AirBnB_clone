b4377e50bb291dccfe3f8198a6237ec68f0f6b58
Mateo <mateog91@gmail.com>
2021-11-04 19:13:05 -0500

    UPD: updated do_create with proper functionalities

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """Contains the entry point of the command interpreter
 """
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,6 +11,12 @@ class HBNBCommand(cmd.Cmd):
     Args:
         cmd (Cmd): Instance or subclass of Cmd module
         for line-oriented command interpreters
+
+    Description:
+        You can assume arguments are always in the right order
+        Each arguments are separated by a space
+        A string argument with a space must be between double quote
+        The error management starts from the first argument to the last one
     """
     prompt = '(hbnb) '
 
@@ -34,6 +41,15 @@ class HBNBCommand(cmd.Cmd):
         """ Creates a new instance of BaseModel, saves it (to the JSON file)
             and prints the id.
         """
+        line_list = arg.split()
+        if (len(line_list) <= 0):
+            print("** class name missing **")
+        else:
+            try:
+                new_object = eval(line_list[0])()
+                print(new_object.id)
+            except NameError:
+                print("** class doesn't exist **")
 
 
 if __name__ == '__main__':
